974d811ac2ceae027816a6f9551ae97daf297b07
Emmanuel <ewuluemmanuel@gmail.com>
2022-09-11 16:53:18 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
+
+#include <stdlib.h>
+
+
+
 /**
-* main - print if the number is  posituve, zero, or negative
-*
-* Description: using the main function
-* this program prints "Programming is positive, zero, or negative
-* Return: 0
-*/
+ *
+ * * main - putchar print lowercase alphabets
+ *
+ *
+ *
+ * * Description: using the main function
+ *
+ * * this program prints Lowercase Alphabets a-z
+ *
+ * * Return: 0
+ *
+ **/
+
 int main(void)
+
 {
-char low;
-for (low = 'a' ; low <= 'z' ; low++)
-{
-putchar(low);
-for (low = 'A' ; low <= 'Z' ; low++)
-}
-putchar(low);
-putchar('\n');
-return (0);
+
+	int i;
+
+	for (i = 97; i <= 122; i++)
+
+	{
+
+		putchar(i);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
 }
