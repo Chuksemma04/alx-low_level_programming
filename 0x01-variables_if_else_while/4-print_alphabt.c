f4b2cb6ec02626893b3a8d965371c176f9d84ea0
Emmanuel <ewuluemmanuel@gmail.com>
2022-09-11 17:03:11 -0700

    print alphabet 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+
+
+/**
+ *
+ *  * main - putchar print Uppercase and lowercase alphabets
+ *
+ *   *
+ *
+ *    * Description: using the main function
+ *
+ *     * this program prints Uppercase and Lowercase Alphabets
+ *
+ *      * Return: 0
+ *
+ *       */
+
+
+
+int main(void)
+
+{
+
+	int c;
+
+
+
+	for (c = 97; c < 123; c++)
+
+	{
+
+		if (c != 101 && c != 113)
+
+		{
+
+			putchar(c);
+
+		}
+
+
+
+	}
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
