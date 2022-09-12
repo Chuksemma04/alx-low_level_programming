c480d6c991f9a5d3c96c4a8d2e3a5932775d5758
Emmanuel <ewuluemmanuel@gmail.com>
2022-09-12 03:22:45 -0700

    5 print alpha

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+
+/*
+ *
+ *  * main - Write a program that prints all single digit numbers of base 10
+ *
+ *  
+ *
+ *    * Description: using the main function
+ *
+ *     * this program prints Uppercase and Lowercase Alphabets
+ *
+ *      * Return: 0
+ *
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+
+
+	for (i = 0; i < 10; i++)
+
+	{
+
+		printf("%d", i);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
