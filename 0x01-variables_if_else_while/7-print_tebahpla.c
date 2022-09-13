9fa252e992023b8d84d9bb9f89b63339a61278b0
Emmanuel <ewuluemmanuel@gmail.com>
2022-09-12 18:11:32 -0700

    print tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 
-/*
+
+/**
+ * main - putchar print lowercase in reverse order
  *
- *  * main - putchar print lowercase in reverse order
- *
- *    * Description: using the main function
- *
- *     * this program prints base 10 numbers
- *
- *      * Return: 0
- *
+ * Description: using the main function
+ * this program prints base 10 numbers
+ * Return: 0
  */
-
-
-
 int main(void)
-
 {
 
 	int i;
