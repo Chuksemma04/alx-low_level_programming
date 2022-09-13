1b0de4a109294b72760718e26fe749615b79a9da
Emmanuel <ewuluemmanuel@gmail.com>
2022-09-12 18:02:21 -0700

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,17 +2,12 @@
 
 
 /**
- *
  * main - Write a program that prints all single digit numbers of base 10
  *
  * Description: prints Uppercase and Lowercase Alphabets
- *
  * Return: Always 0 (Success)
- *
  */
-
 int main(void)
-
 {
 
 	int i;
