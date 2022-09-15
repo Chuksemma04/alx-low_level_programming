3ea9ac8dddf9fc02b58412983ffc77f77c6a5405
Emmanuel <ewuluemmanuel@gmail.com>
2022-09-15 03:17:59 -0700

    Print remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -25,12 +25,12 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("invalid date: %2d/%02d/%04d\n", month, day - 31, year);
+		printf("invalid date: %2d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
